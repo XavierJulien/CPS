a38f41f406c17486d561de3f8008ee5f173598fb
XavierJulien <julienxvier774@github.com>
2019-04-16 14:37:03 +0200

    Fin spécification examen

--- Projet/LodeRunner/src/loderunner/services/CharacterService.java
+++ Projet/LodeRunner/src/loderunner/services/CharacterService.java
@@ -2,7 +2,72 @@ package loderunner.services;
 
 public interface CharacterService {
 	/* Observators */
+	public EnvironnementService getEnvi();
+	public int getHeight();
+	public int getWidth();
+	
+	
 	/* Constructors */
+	/**
+	 * pre : init(h,w) require 0<h && 0<w
+	 * post : getHeight(init(h,w)) == h 
+	 * 	 	  getWidth(init(h,w)) == w
+	 * 		  \forall x: Integer \in [0..getWidth()-1]
+	 * 		  	\forall y: Integer \in [0..getHeight()-1]
+	 * 			  getCellNature(init(h,w),x,y) == EMP
+	 */
+	public void init(ScreenService s,int height,int width);
+	
+	
 	/* Invariant */
+	/**
+	 * Environnement::CellNature(getEnvi(),getWidth(),getHeight()) \in {Cell.EMP,Cell.HOL,Cell.LAD,Cell.HDR}
+	 * Environnement::CellNature(getEnvi(),getWidth(),getHeight()) \in {Cell.EMP,Cell.HOL,Cell.LAD,Cell.HDR}
+	 */
+		
+	
 	/* Operators */
+	/**
+	 * post : getHeigth(goLeft()) = getHeight()
+	 *		  getWidth() == 0 \implies getWidth(goLeft()) == getWidth()
+	 *		  Environnement::CellNature(getEnvi(),getWidth()-1,getHeight()) \in {Cell.MTL,Cell.PLT,Cell.LAD}
+	 *		  	\implies getWidth(goLeft()) == getWidth()
+	 *		  Environnement::CellNature(getEnvi(),getWidth(),getHeight()) \not \in {Cell.HDR,Cell.LAD} &&
+	 *	   	  	Environnement::CellNature(getEnvi(),getWidth(),getHeight()-1) \not \in {Cell.MTL,Cell.PLT} &&
+	 *	   	  	\not \exists c : Character \in Environment::CellContent(getEnvi(),getWidth(),getHeight()-1)
+	 *	      		\implies getWidth(goLeft()) == getWidth()
+	 *		  \exists c : Character \in Environment::CellContent(getEnvi(),getWidth()-1,getHeight())
+	 *			\implies getWidth(goLeft()) == getWidth()
+	 *		  getWidth() != 0 &&
+	 *			Environnement::CellNature(getEnvi(),getWidth()-1,getHeight()) \not \in {Cell.MTL,Cell.PLT} &&
+	 *			Environnement::CellNature(getEnvi(),getWidth(),getHeight()) \in {Cell.LAD,Cell.HDR} ||
+	 *			Environnement::CellNature(getEnvi(),getWidth(),getHeight()-1) \in {Cell.MTL,Cell.PLT,Cell.LAD} ||
+	 *			\exists c : Character \in Environment::CellContent(getEnvi(),getWidth(),getHeight()-1) &&
+	 *			\not \exists c : Character \in Environment::CellContent(getEnvi(),getWidth()-1,getHeight())
+	 *			\implies getWidth(goLeft()) == getWidth()-1
+	 *		  
+	 */
+	public void goLeft();
+	public void goRight();
+	public void goUp();
+	public void goDown();
+	/**
+	 * pre : dig(x,y) require getCellNature(x,y) == PLT
+	 * post : getCellNature(dig(x,y),x,y) == HOL
+	 * 		  \forall x: Integer \in [0..getWidth()-1]
+	 * 		  	\forall y: Integer \in [0..getHeight()-1]
+	 * 			  \with (x,y)!=(u,v)
+	 * 			    \implies getCellNature(dig(u,v),x,y) == CellNature(x,y)
+	 */
+	public void dig(int x, int y);
+	
+	/**
+	 * pre : fill(x,y) require getCellNature(x,y) == HOL
+	 * post : getCellNature(fill(x,y),x,y) == PLT
+	 * 		  \forall x: Integer \in [0..getWidth()-1]
+	 * 		  	\forall y: Integer \in [0..getHeight()-1]
+	 * 			  \with (x,y)!=(u,v)
+	 * 			    \implies getCellNature(fill(u,v),x,y) == CellNature(x,y)
+	  */
+	public void fill(int x, int y);
 }
--- Projet/LodeRunner/src/loderunner/services/EnvironnementService.java
+++ Projet/LodeRunner/src/loderunner/services/EnvironnementService.java
@@ -1,5 +1,24 @@
 package loderunner.services;
 
+import java.util.Set;
+
 public interface EnvironnementService extends ScreenService{
+	/* Observators */
+	/**
+	 * pre : getCellContent(x,y) require 0<=x<getHeight() && require 0<=y<getHeight() (super?)
+	 */
+	public Set<Character> getCellContent(int x,int y);
+	
+	/* Invariant */
+	/**
+	 *	\forall (x,y) : Integer * Integer \in [0..getWidth()-1]*[0..getHeight()-1]
+	 *		\forall (c1,c2) : Character * Character \in getCellContent(x,y), c1 == c2
+	 * 	\forall (x,y) : Integer * Integer \in [0..getWidth()-1]*[0..getHeight()-1] 
+	 * 		\if getCellNature(x,y) \in {Cell.MTL,Cell.PLR} \implies getCellContent(x,y) == \void
+	 * 	\forall (x,y) : Integer * Integer \in [0..getWidth()-1]*[0..getHeight()-1] 
+	 * 		\exists t : Treasure \in getCellContent(x,y)
+	 * 			\implies getCellNature(x,y) = Cell.EMP && getCellNature(x,y-1) \in {Cell.MTL,Cell.PLT}
+	 */
+		
 
 }
